82055b8d21a85149cf1aff6e90c6a0c78e712634
Tyler Chamberlain <hexarobi@gmail.com>
2023-01-17 18:40:53 -0600

    Add files via upload

---
+++ lib/ScriptManager/script_repository.lua
@@ -0,0 +1,127 @@
+-- ScriptManager - Script Repository
+
+local SCRIPT_VERSION = "0.6"
+
+local scripts_repository = {
+    {
+        name = "AcJokerScript",
+        author = "AcJoker",
+        description = "Lots of useful and fun options, custom built by AcJoker.",
+        project_url = "https://github.com/acjoker8818/AcjokerScript",
+        discord_url = "https://discord.gg/fn4uBbFNnA",
+        install_config={
+            source_url="https://raw.githubusercontent.com/acjoker8818/AcjokerScript/main/AcjokerScript.lua",
+            script_relpath="AcjokerScript.lua",
+        }
+    },
+    {
+        name="Andy's Script",
+        author="Andy",
+        description="AndyScript for Stand GTA V mod menu",
+        project_url="https://github.com/Lancito01/AndyScript",
+        discord_url="https://discord.gg/9vzATnaM9c",
+    },
+    {
+        name="BallDrop",
+        author="BigTuna76",
+        description="A silly little script that allows you to unleash a torrent of oversized soccer balls from above on yourself or another player",
+        project_url="https://github.com/BigTuna76/stand-lua-balldrop",
+    },
+    {
+        name="Constructor",
+        author="Hexarobi",
+        description="Load, edit, and create custom vehicles, maps and skins.",
+        project_url = "https://github.com/hexarobi/stand-lua-constructor",
+        discord_url = "https://discord.gg/2u5HbHPB9y",
+    },
+    {
+        name="CollectibleHelper",
+        author="aaronlink127",
+        description="Shows blips on the map for collectibles.",
+        install_config={
+            source_url="https://raw.githubusercontent.com/hexarobi/stand-lua-script-repository/main/CollectibleHelper.lua",
+            script_relpath="CollectibleHelper.lua",
+        }
+    },
+    {
+        name="GameCrunchScript",
+        author="GameCrunch",
+        description="real.",
+        project_url = "https://github.com/Game-Crunch/GameCrunchScript",
+        discord_url = "https://discord.gg/8eGrTbQYce",
+    },
+    {
+        name="HeistControl",
+        author="IceDoomfist",
+        description="Complete your heists with ease.",
+        project_url = "https://github.com/IceDoomfist/Stand-Heist-Control",
+        discord_url = "https://discord.gg/KTFAYQn5Xz",
+    },
+    {
+        name="HornSongs",
+        author="Hexarobi",
+        description="Play musical song files using your vehicle horn.",
+        project_url = "https://github.com/hexarobi/stand-lua-hornsongs",
+        discord_url = "https://discord.gg/2u5HbHPB9y",
+    },
+    {
+        name="Jinxscript",
+        author="Prisuhm",
+        description="A must-have script full of great options.",
+        discord_url = "https://discord.gg/7AYFEtCXwm",
+        install_config={
+            source_url="https://raw.githubusercontent.com/hexarobi/stand-lua-script-repository/main/JinxScript.lua",
+            script_relpath="JinxScript.lua",
+        }
+    },
+    {
+        name="MusinessBanagement",
+        author="ICYPhoenix and Ren",
+        description="Original MB project file, slightly different from the version now found in Stand repo.",
+        discord_url = "https://discord.gg/KYav78KmNc",
+        install_config={
+            source_url="https://raw.githubusercontent.com/hexarobi/stand-lua-script-repository/main/MusinessBanagersource.lua",
+            script_relpath="MusinessBanagersource.lua",
+            script_run_name="MusinessBanagersource"
+        }
+    },
+    {
+        name="next-translate",
+        author="Lance",
+        description="Next-Translate is a chat translator script that is as simple and intelligent as possible",
+        project_url="https://github.com/xSetrox/next-translate",
+    },
+    {
+        name = "Policify",
+        author = "Hexarobi",
+        description = "Turns any vehicle into a police vehicle, with controllable flashing lights and sirens.",
+        project_url = "https://github.com/hexarobi/stand-lua-policify",
+        discord_url = "https://discord.gg/2u5HbHPB9y",
+    },
+    {
+        name = "RTL: REPLICA Theme Loader",
+        author = "ukn",
+        description = "Load a Stand menu theme from several that replicate other menus look and feel.",
+        project_url = "https://github.com/ukn-repos/rtl",
+        discord_url = "https://discord.gg/N43aKNDJyF",
+    },
+    {
+        name="SlotBot",
+        author="Hexarobi",
+        description="Automatic spinning of casino slot machine to make a quick $50mil per day.",
+        project_url = "https://github.com/hexarobi/stand-lua-slotbot",
+        discord_url = "https://discord.gg/2u5HbHPB9y",
+    },
+    {
+        name="Undefined",
+        author="Undefined Pony",
+        description="The script mainly features aim actions, aim teleport, aim beam, online players list and player ESP, but it also has other cool features.",
+        project_url="https://gitlab.com/undefinedscripts/undefined-for-stand",
+        install_config={
+            source_url="https://gitlab.com/undefinedscripts/undefined-for-stand/-/raw/main/Undefined.lua",
+            script_relpath="Undefined.lua",
+        }
+    },
+}
+
+return scripts_repository
